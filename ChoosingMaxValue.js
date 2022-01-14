7a6af5ac89b082c7f6ec003996f428a0afb4a499
Manar <manarhoms21@gmail.com>
2022-01-14 16:07:34 +0300

    Update ChoosingMaxValue.js

--- ChoosingMaxValue.js
+++ ChoosingMaxValue.js
@@ -1,14 +1,14 @@
 //for refrence: https://appointment.visafg.com/appointment/confirm
-let selected_element = document.getElementById("slot");
+let slotField = document.getElementById("slot");
 let myArray = [];
-for (let i = 1; i < selected_element.length; i++) {
+for (let i = 1; i < slotField.length; i++) {
   appointmentCountPerDay = Number(
-    selected_element[i].innerText.slice(-2).replace(" ", "")
+    slotField[i].innerText.slice(-2).replace(" ", "")
   );
   myArray.push(appointmentCountPerDay);
 
   let maxValue = Math.max.apply(Math, myArray);
   if (appointmentCountPerDay == maxValue) {
-    selected_element.selectedIndex = i;
+    slotField.selectedIndex = i;
   }
 }
