9ec170fbeac517008d087bf5af2276bffd4674e8
Manar <manarhoms21@gmail.com>
2022-01-14 15:35:41 +0300

    Add files via upload

---
+++ ChoosingMaxValue.js
@@ -0,0 +1,13 @@
+let selected_element = document.getElementById("slot");
+let myArray = [];
+for (let i = 1; i < selected_element.length; i++) {
+  appointmentCountPerDay = Number(
+    selected_element[i].innerText.slice(-2).replace(" ", "")
+  );
+  myArray.push(appointmentCountPerDay);
+
+  let maxValue = Math.max.apply(Math, myArray);
+  if (appointmentCountPerDay == maxValue) {
+    selected_element.selectedIndex = i;
+  }
+}
